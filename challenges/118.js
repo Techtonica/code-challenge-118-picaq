524a3029bbcc9afae8874b778881092e786ebccf
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-17 09:35:26 -0800

    complete 118.js Parrot Class Revisited by extending Parrot and modifying speak()

--- challenges/118.js
+++ challenges/118.js
@@ -53,20 +53,44 @@
 
 
 class Parrot {
- // ...
-}
+    constructor( named = "A nameless parrot" ) {
+      this._named = named;
+      this._speak = named + " want a cracker!";
+    }
+    get name() {
+      return this._name;
+    }
+    speak() {
+      return this._speak;
+    }
+  }
+  
+  
+  // let polly = new Parrot("Polly");
+  // polly.speak(); // returns "Polly want a cracker!"
+  
+  // let benStiller = new Parrot("Ben");
+  // benStiller.speak();  // returns "Ben want a cracker!"
+  
+  // let anonymous = new Parrot();
+  // anonymous.speak();  // returns "A nameless parrot want a cracker!"
+  
 
 class ApprenticeParrot extends Parrot {
- // ...
+
+    speak() {
+        // return this._speak;
+        return "😴";
+    }
 }
 
-let polly = new Parrot("Polly");
-polly.speak();
+// let polly = new Parrot("Polly");
+// console.log(polly.speak());
 // returns "Polly want a cracker!"
 
 
-// const me = new ApprenticeParrot("Your Name");
-// me.speak();
+// const me = new ApprenticeParrot("Mandy");
+// console.log(me.speak());
 // return "😴"
 
 module.exports = {Parrot, ApprenticeParrot};
